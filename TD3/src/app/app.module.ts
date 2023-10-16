a76c8009687e0b9e182fff94fde12bdfac178238
Natgru06 <nathan06062002@gmail.com>
2023-10-16 16:23:00 +0200

    Td3 Fin

--- TD3/src/app/app.module.ts
+++ TD3/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LessonEditFormComponent } from './lesson-edit-form/lesson-edit-form.component';
+import { LessonSearchPageComponent } from './lesson-search-page/lesson-search-page.component';
+import { MenuNavBarComponent } from './menu-nav-bar/menu-nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LessonEditFormComponent,
+    LessonSearchPageComponent,
+    MenuNavBarComponent
   ],
   imports: [
     BrowserModule,
